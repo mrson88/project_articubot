e8f334c30884c6aaa3c5527cd51267d551462437
mrson88 <sonk9d@gmail.com>
2024-07-26 10:45:16 +0700

    3

--- src/robot_recognition/scripts/robot_ros2_reg.py
+++ src/robot_recognition/scripts/robot_ros2_reg.py
@@ -16,7 +16,7 @@ from geometry_msgs.msg import Point
 from articubot_msgs.action import ArticubotTask
 from rclpy.action import ActionClient
 from rclpy.action.client import ClientGoalHandle, GoalStatus
-from Facerec_QT.face_rec import FaceRecognition,read_db,write_db
+# from Facerec_QT.face_rec import FaceRecognition,read_db,write_db
 import numpy as np
 from geometry_msgs.msg import PointStamped, PoseStamped
 from tf_transformations import quaternion_from_euler
@@ -85,7 +85,7 @@ class Camera_subscriber(Node):
         self.frame_height = 480
         self.frame_width = 640
         self.depth_image=[]
-        self.face_recognition = FaceRecognition(0.7, self.frame_height, self.frame_width)
+        # self.face_recognition = FaceRecognition(0.7, self.frame_height, self.frame_width)
         self.findball = False
 
     def camera_info_callback(self, msg):

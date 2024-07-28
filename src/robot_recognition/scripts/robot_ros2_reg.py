9e06eafd9deb62811a3caf4a83cd7c77174fb380
mrson88 <sonk9d@gmail.com>
2024-07-28 17:45:38 +0700

    31

--- src/robot_recognition/scripts/robot_ros2_reg.py
+++ src/robot_recognition/scripts/robot_ros2_reg.py
@@ -32,7 +32,7 @@ class Camera_subscriber(Node):
 
         self.subscription = self.create_subscription(
             Image,
-            '/camera/camera/color/image_raw',
+            'color/image_raw',
             self.camera_callback,
             1)
         # self.subscription = self.create_subscription(
@@ -43,7 +43,7 @@ class Camera_subscriber(Node):
         self.subscription 
         self.subscription_depth = self.create_subscription(
             Image,
-            '/camera/camera/depth/image_rect_raw',
+            'depth/image_rect_raw',
             self.depth_camera_callback,
             1)
         # self.create_subscription(

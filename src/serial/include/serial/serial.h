4f4ae19c6faa3c22840a0104261b1792b764699a
mrson88 <sonk9d@gmail.com>
2024-07-28 19:58:02 +0700

    321

--- src/serial/include/serial/serial.h
+++ src/serial/include/serial/serial.h
@@ -178,7 +178,7 @@ public:
    * \throw std::invalid_argument
    */
   Serial (const std::string &port = "",
-          uint32_t baudrate = 9600,
+          uint32_t baudrate = 57600,
           Timeout timeout = Timeout(),
           bytesize_t bytesize = eightbits,
           parity_t parity = parity_none,

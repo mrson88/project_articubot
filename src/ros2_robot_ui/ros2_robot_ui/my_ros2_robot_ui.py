7f23f1e6bd84a0acdcb64140a3db8c4b82e84fc0
mrson88 <sonk9d@gmail.com>
2024-08-03 10:04:28 +0700

    eq

--- src/ros2_robot_ui/ros2_robot_ui/my_ros2_robot_ui.py
+++ src/ros2_robot_ui/ros2_robot_ui/my_ros2_robot_ui.py
@@ -364,7 +364,7 @@ class MainWindow(QMainWindow):
 
 
     def move_to_home(self):
-        self.node.send_goal_robot_arm(self.point[0],self.point[1],self.point[2],self.point[3],self.point[4],self.point[5],0)
+        self.node.send_goal_robot_arm(0.5,1.2,1.2,1.2,0,0,1)
 
     def move_to_position1(self):
         self.publish_joint_states([0.5, -0.3, 0.2, 0.1, 0.0, 0.0])

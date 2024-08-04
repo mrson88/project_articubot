b4bcd63cdb6ad2de04bb414ad9ba29979a5a8e63
mrson88 <sonk9d@gmail.com>
2024-08-05 05:01:36 +0700

    312

--- src/ROSArduinoBridge/servos.h
+++ src/ROSArduinoBridge/servos.h
@@ -22,7 +22,7 @@ Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 // Decreasing this number will make the servo sweep more quickly.
 // Zero is the default number and will make the servos spin at
 // full speed.  150 ms makes them spin very slowly.
-int stepDelay [N_SERVOS] = { 50, 50 , 50, 50, 50, 50}; // ms
+int stepDelay [N_SERVOS] = { 0, 0 , 0, 0, 0, 0}; // ms
 
 // Pins
 byte servoPins [N_SERVOS] = { 0, 1, 2 , 3, 4, 5};

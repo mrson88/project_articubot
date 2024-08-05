bed2aac35e75e4bbd4857b4fcbb7ad11be61cfa3
mrson88 <sonk9d@gmail.com>
2024-08-05 22:37:16 +0700

    312

--- src/articubot_remote/src/test_server.cpp
+++ src/articubot_remote/src/test_server.cpp
@@ -498,9 +498,8 @@ private:
         RCLCPP_INFO(get_logger(), "Cartesian path (%.2f%% achieved)", fraction * 100.0);
         my_plan.trajectory_ = trajectory;
         success = true;
-        // Mở gripper
-        move_group_gripper_interface->setNamedTarget("open");
-        move_group_gripper_interface->move();
+
+
         move_group_interface->execute(my_plan);
       } else {
         RCLCPP_ERROR(get_logger(), "Cartesian path planning failed");
@@ -530,8 +529,9 @@ private:
     // Xử lý các task cụ thể (giữ nguyên phần này từ mã gốc của bạn)
     switch (goal_handle->get_goal()->task) {
       case 0:
-
-        
+        // Mở gripper
+        move_group_gripper_interface->setNamedTarget("open");
+        move_group_gripper_interface->move();       
         // Đóng gripper
         gripper_joint_values = {GRIPPER_CLOSE};
         move_group_gripper_interface->setNamedTarget("close");

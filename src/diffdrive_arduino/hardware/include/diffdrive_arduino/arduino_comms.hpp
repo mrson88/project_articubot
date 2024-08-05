c703ebf016e5dbdf3df762863a02a7b599612478
mrson88 <sonk9d@gmail.com>
2024-08-05 23:12:21 +0700

    321

--- src/diffdrive_arduino/hardware/include/diffdrive_arduino/arduino_comms.hpp
+++ src/diffdrive_arduino/hardware/include/diffdrive_arduino/arduino_comms.hpp
@@ -13,16 +13,16 @@ LibSerial::BaudRate convert_baud_rate(int baud_rate)
   // Just handle some common baud rates
   switch (baud_rate)
   {
-    case 1200: return LibSerial::BaudRate::BAUD_1200;
-    case 1800: return LibSerial::BaudRate::BAUD_1800;
-    case 2400: return LibSerial::BaudRate::BAUD_2400;
-    case 4800: return LibSerial::BaudRate::BAUD_4800;
-    case 9600: return LibSerial::BaudRate::BAUD_9600;
-    case 19200: return LibSerial::BaudRate::BAUD_19200;
-    case 38400: return LibSerial::BaudRate::BAUD_38400;
+    // case 1200: return LibSerial::BaudRate::BAUD_1200;
+    // case 1800: return LibSerial::BaudRate::BAUD_1800;
+    // case 2400: return LibSerial::BaudRate::BAUD_2400;
+    // case 4800: return LibSerial::BaudRate::BAUD_4800;
+    // case 9600: return LibSerial::BaudRate::BAUD_9600;
+    // case 19200: return LibSerial::BaudRate::BAUD_19200;
+    // case 38400: return LibSerial::BaudRate::BAUD_38400;
     case 57600: return LibSerial::BaudRate::BAUD_57600;
-    case 115200: return LibSerial::BaudRate::BAUD_115200;
-    case 230400: return LibSerial::BaudRate::BAUD_230400;
+    // case 115200: return LibSerial::BaudRate::BAUD_115200;
+    // case 230400: return LibSerial::BaudRate::BAUD_230400;
     default:
       std::cout << "Error! Baud rate " << baud_rate << " not supported! Default to 57600" << std::endl;
       return LibSerial::BaudRate::BAUD_57600;

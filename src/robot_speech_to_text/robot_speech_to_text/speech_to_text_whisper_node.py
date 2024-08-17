8f541e744fa3fe7e729b2fc8d4ea1d3a510a59b4
mrson88 <sonk9d@gmail.com>
2024-08-17 16:19:31 +0700

    323

--- src/robot_speech_to_text/robot_speech_to_text/speech_to_text_whisper_node.py
+++ src/robot_speech_to_text/robot_speech_to_text/speech_to_text_whisper_node.py
@@ -61,7 +61,7 @@ class Speech_Whisper_Node(Node):
 
                     if voice_activity_detected:
                         frames.append(data)            
-                        if current_noise_level < ambient_noise_level + 200:
+                        if current_noise_level < ambient_noise_level + 400:
                             break 
 
                     if not voice_activity_detected and current_noise_level > long_term_noise_level + 600:

d4f2092aa314ac0b3bf1bde5a17c9a8454754bfd
mrson88 <sonk9d@gmail.com>
2024-08-17 17:20:54 +0700

    323

--- src/robot_speech_to_text/robot_speech_to_text/speech_to_text_whisper_node.py
+++ src/robot_speech_to_text/robot_speech_to_text/speech_to_text_whisper_node.py
@@ -63,10 +63,10 @@ class Speech_Whisper_Node(Node):
 
                     if voice_activity_detected:
                         frames.append(data)            
-                        if current_noise_level < ambient_noise_level + 300:
+                        if current_noise_level < ambient_noise_level + 400:
                             break # voice activity ends 
 
-                    if not voice_activity_detected and current_noise_level > long_term_noise_level + 600:
+                    if not voice_activity_detected and current_noise_level > long_term_noise_level + 700:
                         voice_activity_detected = True
                         print("I'm all ears.\n")
                         ambient_noise_level = long_term_noise_level

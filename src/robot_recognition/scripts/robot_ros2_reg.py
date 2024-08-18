2cc008a64f047c35ef2be4c5e899a676e5d8880e
mrson88 <sonk9d@gmail.com>
2024-08-18 08:49:51 +0700

    33

--- src/robot_recognition/scripts/robot_ros2_reg.py
+++ src/robot_recognition/scripts/robot_ros2_reg.py
@@ -62,10 +62,10 @@ class CameraSubscriber(Node):
 
     def setup_model(self):
         package_share_dir = get_package_share_directory("robot_recognition")
-        model_dir = os.path.join(package_share_dir, "scripts", "yolov8m.pt")
+        # model_dir = os.path.join(package_share_dir, "scripts", "yolov8m.pt")
+        # self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        # self.model = YOLO(model_dir).to(self.device)
         model_engine_onnx_dir = os.path.join(package_share_dir, "scripts", "yolov8m.engine")
-        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        self.model = YOLO(model_dir).to(self.device)
         self.model=YOLO(model_engine_onnx_dir,task='detect')
 
     def setup_variables(self):
